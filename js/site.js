9f27f0a67ead32c8b6674dd4c7a9fcffb218a644
Qi Li <cloudbsd@gmail.com>
2014-01-29 04:30:37 +0800

    use 2014.01.26 version.

--- js/site.js
+++ js/site.js
@@ -33,13 +33,99 @@ $(document).ready(function(){
                   } 
     } 
 
-    //选项卡特效
-    function setTab(name,cursel,n){
-     for(i=1;i<=n;i++){
-      var menu=document.getElementById(name+i);
-      var con=document.getElementById("con_"+name+"_"+i);
-      menu.className=i==cursel?"hover":"";
-      con.style.display=i==cursel?"block":"none";
-     }
+
+    //add by rainyang
+    $(".insuranceTab a").on("click",function(){
+        var dataid = $(this).attr("data-id");
+        $(".insuranceType").hide();
+        $(".insuranceTab a").removeClass("hover");
+        $("#con_click_"+dataid).show();
+        $(this).addClass("hover");
+    }); 
+    
+    if($( ".datepicker" ).length > 0){
+        $( ".datepicker" ).datepicker({
+            showOn: "button",
+            buttonImage: "images/calendar.gif",
+            buttonImageOnly: true
+        });
     }
+
+    if($( ".piclist" ).length > 0){
+        $(".piclist").jCarouselLite({
+            btnNext: ".insuranceCobrandingTr",
+            btnPrev: ".insuranceCobrandingTl",
+            auto: 800,
+            speed: 1000,
+            scroll: 1
+        });
+    }
+
+    $("#btn-add-person").on("click",function(){
+        var person = tmpl("tmpl-person-body");
+        $("#add-person").append(person);
+    });
+
+    $(".btn-del-person").click(function(){
+        $(this).parent().parent().find(".bnt").remove();
+    });
+
+    $("#add-person").on("click",".del-person",function(){
+        $(this).parent().parent().remove();
+    });
+
+    $("#recommend").show();
+    $("#bnt-discount").mouseover(function(){
+        $("#discount").show();
+        $("#recommend").hide();
+        $(this).removeClass("hover");
+        $("#bnt-recommend").removeClass("hover");
+        $(this).addClass("hover");
+    });
+
+    $("#bnt-recommend").mouseover(function(){
+        $("#discount").hide();
+        $("#recommend").show();
+        $(this).removeClass("hover");
+        $("#bnt-discount").removeClass("hover");
+        $(this).addClass("hover");
+    });
+
+    $(".nav .nav1, .sub-page-menu").mouseover(function(){
+        $(".order_site").hide();
+        $(".sub-page-menu").show();
+    });
+
+    $(".sub-page-menu").mouseout(function(){
+        $(".order_site").show();
+        $(".sub-page-menu").hide();
+    });
+
+    $(".add-linkman-title").click(function(){
+        $(".add-linkman-form").show();
+    });
+    
+    $("#bnt-linkman-submit").click(function(){
+        $(".add-linkman-form").hide();
+    });
+
+    $(".prod_prop_list").click(function(){
+        var $t = $(this);
+        var $dataid = $t.attr("data-id");
+        var $text= $t.text();
+        $(".my-select").append('<ul><li><strong><label>'+$t.closest("dl").find("dt").text()+'</label>'+$text+'<span></span></strong></li></ul>');
+    });
+
+    $(".lay-prod-navgation").on("click",".my-select span",function(){
+        $(this).closest("ul").remove();
+    });
+
+    $("#dev_clearAllProp").click(function(){
+        $(".my-select").empty();
+    });
+
+    $("#dev_showOrHide").click(function(){
+        $("#dev_MoreFilters").toggle();
+    })
 });
+
